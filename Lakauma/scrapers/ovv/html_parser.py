555175a8e311a32230d4058fee9606552437c1c0
samuel <samuel@MuoviLapyska.(none)>
2013-04-10 09:14:59 +0300

    Lisäilin kansioita ehkä turhaan

---
+++ Lakauma/scrapers/ovv/html_parser.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+# @author: Samuel Uusi-Makela
+# @version: 8.4.2013
+#
+# Module to extract information from html code. Also includes
+# method for returning substring of a string between 
+# two substrings.
+# Modify the keywords to find results from lines containing keywords.
+
+
+from ftfy import fix_text
+
+# Extract data from html string s between two strings start and end.
+def html_ext_between(s, start, end): 
+    """ Extract string from between two defined strings start and end
+        Usage:
+
+        >string_smthing = "<a> hereissomethinguseful </a>"
+        >print html_ext_between(string_smthing, "<a>", "</a>") 
+       
+        hereissomethinguseful
+    """
+    return "lol"
+    
+
+# Extract a substring from a string s which is between defined strings start and end.
+def sub_str_extract(s, start, end):
+    pass
+
+def parseilu():
+    # Insert the page specific keywords here
+    keywords = ["itemTitle","itemPrice","itemSize","itemFree","itemText"]
+    keyword_start = "resultItem"
+    keyword_end = "itemLink"
+    results = []
+    lippu = False
+    test_html = open('ovv_sorsa', 'r')
+    stripped_results = []
+    flag = True
+
+    for line in test_html:
+        if (keyword_end in line and lippu == True):
+            lippu = False
+        if (keyword_start in line and lippu == False):
+            lippu = True    
+        if (lippu == True):    
+            for word in keywords:
+                if (word in line):
+                    results.append(line)
+    for result in results:
+        fixed1 = result.decode('utf-8')
+        fixed2 = fix_text(fixed1)
+        almost = fixed2.strip()
+        better = almost.split('>')
+        even_better = better[1]
+        nearly = even_better.split('<')
+        if(flag == True): 
+            stripped_results.append([nearly[0]])
+            flag = False
+        else:
+            stripped_results[len(stripped_results)-1].append(nearly[0])
+            if('Jyv' in nearly[0]):
+                flag = True
+    return stripped_results
