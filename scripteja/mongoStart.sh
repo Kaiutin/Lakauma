86d2a00cb40013294f7c28648926f64e93b10227
samuel <samuel@MuoviLapyska.(none)>
2013-04-18 09:14:44 +0300

    Päivittelyä

--- scripteja/mongoStart.sh
+++ scripteja/mongoStart.sh
@@ -1,6 +1,10 @@
-cd ~/Documents/lakauma/Lakauma/vuokrailua/vuokra_tiedot/
+# Change working directory to the projects directory
+# which contains the testdata, items.json
+cd /your/path/to/Lakauma/vuokrailua/vuokra_tiedot/
 
-sudo mongod --dbpath ~/Documents/lakauma/ --smallfiles
+# Start up mongodb, change path to where you want the database to be located
+sudo mongod --dbpath ~/where/you/want/your/database/ --smallfiles
 
+# Finally import the contents of the items.json file to the databse.
 mongoimport --collection asunnot --file items.json --jsonArray
 
--- vuokrailua/vuokra_tiedot/models.py
+++ vuokrailua/vuokra_tiedot/models.py
@@ -2,29 +2,16 @@
 from django.db import models
 from django.utils import timezone
 
+
+# This file governs the models that would be used with SQL database,
+# below is one example of such datamodel.
+
 class Kohde_tiedot(models.Model):
     def __unicode__(self):
-        return self.osoite + " " + "neliot:" + self.neliot + " " + " FUCK YEAH" 
+        return self.osoite  
 
     osoite = models.CharField(max_length=200)
     neliot = models.CharField(max_length=200)
     vuokra = models.CharField(max_length=200)
     tyyppi = models.CharField(max_length=200)
-
-
-class Poll(models.Model):
-    question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    def __unicode__(self):
-        return self.question    
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-
-class Choice(models.Model):
-    poll = models.ForeignKey(Poll)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __unicode__(self):
-        return self.choice_text
 
