e52143368f92767b2a762573993cfd8985a2ee6f
Anton Laurila <anjulaur@chaplin.it.jyu.fi>
2013-05-20 17:30:01 +0300

    Mongoajuriin lisätty tuki pääkohteille

--- vuokrailua/vuokra_tiedot/mongo_ajuri.py
+++ vuokrailua/vuokra_tiedot/mongo_ajuri.py
@@ -5,29 +5,52 @@ from pymongo import MongoClient
 # running locally on your computer. 
 
 # Get current apartments data (asunnot) from database.
-#def hae_data(vuokramin=0, vuokramax=100000000, neliomin=0, neliomax=100000):
-#    client = MongoClient()
-#    db = client.test
-#    collection = db.asunnot
-#    return collection.find({ "vuokra": { "$gte": vuokramin, "$lte": #vuokramax }},{_id: 0})
-
 def hae_data(vuokramin, vuokramax, neliomin, neliomax):
     client = MongoClient()
     db = client.test
     collection = db.asunnot
+    collection2 = db.paakohteet
     data = []
+    data2 = []
+    i = 0
     for kohde in collection.find({},{"_id": 0}):
+        i += 1
         try:
             if (int(kohde["vuokra"]) >= vuokramin and  int(kohde["vuokra"]) <= vuokramax and float(kohde["neliot"]) >= neliomin and float(kohde["neliot"]) <= neliomax): 
+                if (str(kohde["tarjoaja"]) == "KOAS"):
+                    data2.append(str(kohde["linkki"]))
+                    print i + 1500
+                    continue
+                    
                 data.append(kohde)
+                print kohde
+                
         except ValueError:
             if(int(kohde["vuokra"]) >= vuokramin and  int(kohde["vuokra"]) <= vuokramax):
-                data.append(kohde)                
+                if (str(kohde["tarjoaja"]) == "KOAS"):
+                    data2.append(str(kohde["linkki"]))
+                    print i + 1700
+                    continue
+                    
+                data.append(kohde)
+                print i + 2000                      
+
+    data2 = data2[:]
+    
+    data2 = list(set(data2))
+    
+    for linkki in data2:
+    
+       kohde = collection2.find_one({"linkki": linkki}, {"_id": 0}) 
+       print kohde
+       data.append(kohde)
+       #print str(kohde["osoite"])
+    
+    
     data = data[:]
     
     return data
-
-    
+   
 def remove():
     client = MongoClient()
     db = client.test
