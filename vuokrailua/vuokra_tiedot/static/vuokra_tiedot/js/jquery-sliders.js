83bdffb428cd7b57e790ff8aae46e56ab0ffabbd
samuel <samuel@MuoviLapyska.(none)>
2013-05-20 11:00:28 +0300

    Lisenssit paikallaan

--- vuokrailua/vuokra_tiedot/static/vuokra_tiedot/js/jquery-sliders.js
+++ vuokrailua/vuokra_tiedot/static/vuokra_tiedot/js/jquery-sliders.js
@@ -1,3 +1,14 @@
+// This file is part of Loukku, an application to display available rental apartments on Google Maps map.
+// Copyright (C) 2013  Anton Laurila, Jari-Matti Kankaanpää, Samuel Uusi-Mäkelä
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// For more information, please refer to LICENCE file found in /Lakauma,
+// or <http://www.gnu.org/licenses/> and GNU Affero General Public License
+
+
 $(function() {
     $( "#slider-range-vuokra" ).slider({
       range: true,
